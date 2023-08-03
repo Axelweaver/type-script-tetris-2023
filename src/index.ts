46811155e34f3a117f136b7551f774fe326e844e
Axel Tkacheff <aptkachev@rolftech.ru>
2023-08-03 17:55:27 +0300

    add comments

--- src/index.ts
+++ src/index.ts
@@ -17,17 +17,11 @@ const keysState: KeyboardState = {
     rotateFigure: false
 };
 
-document.addEventListener(
-    'keydown',
-    eventHandlerKeyDown(keysState)
-);
-document.addEventListener(
-    'keyup',
-    eventHandlerKeyUp(keysState)
-);
-
+// add keyboard event listeners for a keys
+document.addEventListener('keydown', eventHandlerKeyDown(keysState));
+document.addEventListener('keyup', eventHandlerKeyUp(keysState));
+// game variables
 const view = new MainView(CANVAS_ID);
-
 let nextFigure = new GameFigure();
 let figure = new GameFigure();
 let countFrames = 0;
@@ -36,6 +30,7 @@ const fieldMatrix = new GameFieldMatrix();
 const level = 1;
 let score = 0;
 let lines = 0;
+
 // game loop
 function gameLoop (): void {
     view.clearGameField();
@@ -77,33 +72,33 @@ function gameLoop (): void {
         }
         countKeyboardFrames = 0;
     }
-
+    // moving the figure down
     if (++countFrames > GAME_MOVE_PER_FRAMES) {
         if ((figure.rowIndex + figure.height) < GAME_FIELD_ROWS) {
             figure.moveDown();
+            // If the figure rests on the old figures when moving down
             if (fieldMatrix.isCollision(figure)) {
                 figure.moveUp();
                 mergeFigure();
             }
         }
-
+        // If the figure has reached the bottom
         if (figure.rowIndex + figure.height >= GAME_FIELD_ROWS) {
             mergeFigure();
         }
         countFrames = 0;
     }
-
+    // drawing old figures and current
     view.drawFieldMatrix(fieldMatrix);
     view.drawGameFigure(figure);
-
+    // Check field for a full
     if (fieldMatrix.isOver()) {
         showGameOver();
         return;
     }
-
     requestAnimationFrame(() => { gameLoop(); });
 }
-
+// add current figure to the game field matrix
 function mergeFigure (): void {
     fieldMatrix.merge(figure);
     figure = nextFigure;
@@ -112,7 +107,7 @@ function mergeFigure (): void {
     view.drawNextFigure(nextFigure);
 
     const fullRowsCount = fieldMatrix.getFullRowsCount();
-
+    // change the score if there are filled rows
     if (fullRowsCount > 0) {
         score += level * fullRowsCount * 10;
         lines += fullRowsCount;
@@ -125,20 +120,10 @@ function mergeFigure (): void {
 function showGameOver (): void {
     view.drawInfo('GAME OVER', GAME_OVER_COLOR);
 }
-
+// show initial screen
 view.drawGameField();
 view.drawNextFigureField();
-
-// view.drawInfo('GAME OVER', GAME_OVER_COLOR);
-// view.drawSecondaryInfo('secondary info', SECONDARY_TEXT_COLOR);
-// view.drawGameFigure(figure);
 view.drawNextFigure(nextFigure);
 view.drawScoreInfo(level, lines, score);
-// start
+// start game
 gameLoop();
-
-/* I - lightBlue, J - red, L - green, O - blue, S - cyan, T - yellow, Z - pink
-
-                    ■■■      ■■■        ■■         ■■       ■■■         ■■
-     ■■■■             ▀      ▀          ▀▀        ▀▀         ▀           ▀▀
-*/
