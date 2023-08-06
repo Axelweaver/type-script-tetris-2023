728d963c77dd5bdd9008a8101e0ed7cc26878a98
Axel Tkacheff <aptkachev@rolftech.ru>
2023-08-07 00:12:54 +0300

    add to bg music param html5 and new build

--- src/sounds/BackgroundMusic.ts
+++ src/sounds/BackgroundMusic.ts
@@ -3,8 +3,7 @@ import ORIGINAL_THEME_MP3 from './mp3/original-theme.mp3';
 
 const BackgroundMusic = new Howl({
     src: [ORIGINAL_THEME_MP3],
-    // html5: true,
-    // autoplay: true,
+    html5: true,
     loop: true,
     volume: 0.2
 });
