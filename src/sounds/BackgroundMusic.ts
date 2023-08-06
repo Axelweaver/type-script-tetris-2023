71f4ebbeb4300ab4da116a44406d308e95fa1c52
Axel Tkacheff <aptkachev@rolftech.ru>
2023-08-07 00:22:44 +0300

    fix build

--- src/sounds/BackgroundMusic.ts
+++ src/sounds/BackgroundMusic.ts
@@ -3,9 +3,9 @@ import ORIGINAL_THEME_MP3 from './mp3/original-theme.mp3';
 
 const BackgroundMusic = new Howl({
     src: [ORIGINAL_THEME_MP3],
-    html5: true,
+    // html5: true,
     loop: true,
-    volume: 0.2
+    volume: 0.15
 });
 
 export default BackgroundMusic;
